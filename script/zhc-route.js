5791096b6ae64bf5c252ad23985b8a98cd924b80
hades_lcm <licm@yxke12.com>
2017-12-25 20:50:21 +0800

    finish univ job major

--- script/zhc-route.js
+++ script/zhc-route.js
@@ -15,19 +15,34 @@
       window.location.href = url;
   }
 
+  h.openParentPage = function (url, parames) {
+    if ((typeof parames) == 'object') {
+      var i = 0;
+      for(var key in parames){
+        if (i == 0) {
+          url = url + "?" + key + "=" + encodeURI(parames[key]);
+        }else{
+          url = url + "&" + key + "=" + encodeURI(parames[key]);
+        }
+        i++;
+      }
+    }
+    parent.location.href = url;
+  }
+
   h.closePage = function(parames) {
     window.history.back(); 
   }
 
   h.getPageParame = function(key) {
-    var reg = new RegExp("(^|&)"+ key +"=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return  unescape(r[2]); return null;
-    // return request(key);
+    // var reg = new RegExp("(^|&)"+ key +"=([^&]*)(&|$)");
+    // var r = window.location.search.substr(1).match(reg);
+    // if(r!=null)return  unescape(r[2]); return null;
+    return request(key);
   }
 
   function request(paras){
-    var url = location.href;
+    var url = decodeURI(location.href);
     var paraString = url.substring(url.indexOf("?")+1,url.length).split("&");
     var paraObj = {}
     for (i=0; j=paraString[i]; i++){
